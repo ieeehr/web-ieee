1c0adf7c979c9cebbad7da64c0a0ebda64664068
ieeehr <hridwick@ieee.org>
2025-04-19 18:28:56 +0530

    Removed import of static img

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+//import { StaticImage } from "gatsby-plugin-image"
 
 const Footer = () => {
   return (
     <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
       <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
         <div className="md:max-w-md lg:col-span-2">
-          <a href="/" aria-label="Go home" className="inline-flex items-center">
+          {/* <a href="/" aria-label="Go home" className="inline-flex items-center">
             <StaticImage
               src="../images/logo-blue.jpg"
               quality={100}
@@ -16,7 +16,7 @@ const Footer = () => {
               formats={["AUTO", "WEBP", "AVIF"]}
               alt="logo"
             />
-          </a>
+          </a> */}
           <div className="mt-4 lg:max-w-sm">
             <p className="text-sm text-gray-800">
               Thank you for checking us out, hope we could be of help. Please do
